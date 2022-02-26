b887824a6392ae7c3bfb2f51f3c9e45f89a97367
OldZack <wang_z1@denison.edu>
2022-02-26 17:35:09 -0500

    Fix Layout

--- app/src/main/java/com/example/w4_p5/GamePage.java
+++ app/src/main/java/com/example/w4_p5/GamePage.java
@@ -1,6 +1,7 @@
 package com.example.w4_p5;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GamePage extends AppCompatActivity {
+public class GamePage extends AppCompatActivity implements View.OnClickListener{
 
     private TableLayout tlLetters;
     private TextView tvAnswer;
@@ -32,59 +33,77 @@ public class GamePage extends AppCompatActivity {
         ivMan = (ImageView) findViewById(R.id.ivMan);
         tlLetters = (TableLayout) findViewById(R.id.tlLetter);
 
-        for (int row = 0; row < 4; row ++){
-            TableRow currRow = new TableRow(GamePage.this);
-            for (int button = 0; button < 6; button ++){
-                String letter = Character.toString((char) (button+row*6+65));
-                Button btn = new Button(GamePage.this);
-                btn.setText(letter);
-                btn.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-                currRow.addView(btn);
-
-                btn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        gameState = game.round(btn.getText().charAt(0));
-                        tvAnswer.setText(game.getCurrentWord());
-                        switch(game.getPlayerState()){
-                            case 6:
-                                ivMan.setBackgroundResource(R.drawable.a);
-                                break;
-                            case 5:
-                                ivMan.setBackgroundResource(R.drawable.b);
-                                break;
-                            case 4:
-                                ivMan.setBackgroundResource(R.drawable.c);
-                                break;
-                            case 3:
-                                ivMan.setBackgroundResource(R.drawable.d);
-                                break;
-                            case 2:
-                                ivMan.setBackgroundResource(R.drawable.e);
-                                break;
-                            case 1:
-                                ivMan.setBackgroundResource(R.drawable.f);
-                                break;
-                            case 0:
-                                ivMan.setBackgroundResource(R.drawable.g);
-                                break;
-                        }
-
-
-                        if (gameState == 1){
-                            Intent i = new Intent(GamePage.this, MainActivity.class );
-                            i.putExtra("gs", true);
-                            startActivity(i);
-                        }
-                        else if (gameState == -1){
-                            Intent i = new Intent(GamePage.this, MainActivity.class );
-                            i.putExtra("gs", false);
-                            startActivity(i);
-                        }
-                    }
-                });
+        int orientation = this.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            for (int row = 0; row < 5; row++) {
+                TableRow currRow = new TableRow(GamePage.this);
+                for (int button = 0; button < 6; button++) {
+                    String letter = Character.toString((char) (button + row * 6 + 65));
+                    Button btn = new Button(GamePage.this);
+                    btn.setText(letter);
+                    btn.setTag(letter);
+                    btn.setLayoutParams(new TableRow.LayoutParams(180, 95));
+                    currRow.addView(btn);
+                    btn.setOnClickListener(this);
+                }
+                tlLetters.addView(currRow);
             }
-            tlLetters.addView(currRow);
+            TableRow currRow = new TableRow(GamePage.this);
+        }
+        else{
+            for (int row = 0; row < 4; row++) {
+                TableRow currRow = new TableRow(GamePage.this);
+                for (int button = 0; button < 8; button++) {
+                    String letter = Character.toString((char) (button + row * 8 + 65));
+                    Button btn = new Button(GamePage.this);
+                    btn.setText(letter);
+                    btn.setTag(letter);
+                    btn.setTextSize(12);
+                    btn.setLayoutParams(new TableRow.LayoutParams(150, 90));
+                    currRow.addView(btn);
+                    btn.setOnClickListener(this);
+                }
+                tlLetters.addView(currRow);
+            }
+        }
+    }
+
+    public void onClick(View v) {
+        gameState = game.round(v.getTag().toString().charAt(0));
+        tvAnswer.setText(game.getCurrentWord());
+        switch (game.getPlayerState()) {
+            case 6:
+                ivMan.setImageResource(R.drawable.a);
+                break;
+            case 5:
+                ivMan.setImageResource(R.drawable.b);
+                break;
+            case 4:
+                ivMan.setImageResource(R.drawable.c);
+                break;
+            case 3:
+                ivMan.setImageResource(R.drawable.d);
+                break;
+            case 2:
+                ivMan.setImageResource(R.drawable.e);
+                break;
+            case 1:
+                ivMan.setImageResource(R.drawable.f);
+                break;
+            case 0:
+                ivMan.setImageResource(R.drawable.g);
+                break;
+        }
+
+
+        if (gameState == 1) {
+            Intent i = new Intent(GamePage.this, MainActivity.class);
+            i.putExtra("gs", true);
+            startActivity(i);
+        } else if (gameState == -1) {
+            Intent i = new Intent(GamePage.this, MainActivity.class);
+            i.putExtra("gs", false);
+            startActivity(i);
         }
     }
 
